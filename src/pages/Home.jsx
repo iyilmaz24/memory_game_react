eb284cdff72836a27064c2ddc291dffe8e3010c1
Irfan Yilmaz <iy.usa@outlook.com>
2024-01-05 20:16:11 -0500

    add skeleton of game, use chakra ui card components

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import NavButtons from "../components/NavButtons";
-
+import { Card, CardBody, CardFooter, Image, Stack, Heading, Text, Button, Divider } from '@chakra-ui/react';
 
 function Home() {
 
@@ -15,9 +15,68 @@ function Home() {
     return (
     <>
 
-        <div>Home.jsx</div>
+    <div className="w-screen h-screen flex flex-col place-content-evenly">
+
+        <div className="flex justify-evenly">
+
+            <Card maxW='sm'>
+                <CardBody>
+                    <Image
+                    src='https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
+                    alt='Green double couch with wooden legs'
+                    borderRadius='lg'
+                    />
+                    <Stack mt='6' spacing='3'>
+                    <Heading size='md'>Current Car Name</Heading>
+                    <Text>
+                        Short description of current car.
+                    </Text>
+                    <Text color='blue.600' fontSize='2xl'>
+                        $ ???
+                    </Text>
+                    </Stack>
+                </CardBody>
+                <Divider />
+                <CardFooter className="justify-center">
+                    <Button variant='solid' colorScheme='blue'>
+                        Greater In Price
+                    </Button>
+                </CardFooter>
+            </Card>
+
+
+            <Card maxW='sm'>
+                <CardBody>
+                    <Image
+                    src='https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
+                    alt='Green double couch with wooden legs'
+                    borderRadius='lg'
+                    />
+                    <Stack mt='6' spacing='3'>
+                    <Heading size='md'>Current Car Name</Heading>
+                    <Text>
+                        Short description of current car.
+                    </Text>
+                    <Text color='blue.600' fontSize='2xl'>
+                        $ ???
+                    </Text>
+                    </Stack>
+                </CardBody>
+                <Divider />
+                <CardFooter className="justify-center">
+                    <Button variant='solid' colorScheme='blue'>
+                        Greater In Price
+                    </Button>
+                </CardFooter>
+            </Card>
+
+        </div>
 
         <NavButtons currPage='home' />
+
+
+    </div>
+
 
     </>
     )
